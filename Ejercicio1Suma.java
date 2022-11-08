0bbfcfc88da6b621e3396c13a65c762620983a8d
Agustin Panella <apanella17@gmail.com>
2022-11-07 22:13:16 -0300

    Primer ejercicio, OpenBootcam con java, suma

---
+++ Ejercicio1Suma.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.ejercicio1;
+//import java.util.ArrayList;
+/**
+ *
+ * @author apane
+ */
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+       int primero = 10;
+       int segundo = 5;
+       int tercero = 15;
+        
+        
+       int resultado = SUMA(primero,segundo,tercero);
+       System.out.print(resultado);
+        }
+       static int SUMA(int a,int b,int c){
+           return a + b + c;
+       }
+        
+}
